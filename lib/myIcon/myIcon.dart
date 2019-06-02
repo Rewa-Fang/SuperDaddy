92ee5b5c530694e3670670fd4f2bc703f62596c9
ruihua.fang.o <ruihua.fang.o@nio.com>
2019-06-02 13:37:01 +0800

    日记列表页构建完善

--- lib/myIcon/myIcon.dart
+++ lib/myIcon/myIcon.dart
@@ -4,6 +4,13 @@ class MyIcon {
   static const IconData wechat = const _MyIconData(0xe758);
   static const IconData qq = const _MyIconData(0xe601);
   static const IconData weibo = const _MyIconData(0xe611);
+  static const IconData naiping = const _MyIconData(0xe637);
+  static const IconData home = const _MyIconData(0xe603);
+  static const IconData user = const _MyIconData(0xe602);
+  static const IconData commnet = const _MyIconData(0xe604);
+  static const IconData camera_add = const _MyIconData(0xe600);
+  static const IconData diary = const _MyIconData(0xe63f);
+  static const IconData naiping2 = const _MyIconData(0xe607);
 }
 
 class _MyIconData extends IconData {
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:super_daddy/model/diary_model.dart';
+import 'package:super_daddy/myIcon/myIcon.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,7 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var _diaryDataList = [1, 2, 3, 4, 5];
+  var _diaryDataList = [1, 2, 3, 4, 5, 11, 22, 33, 44, 55, 16, 27, 38, 49, 51];
 
   // @override
   // void initState() {
@@ -29,7 +30,7 @@ class _HomePageState extends State<HomePage> {
             onPressed: () {},
           ),
           IconButton(
-            icon: Icon(Icons.camera_alt),
+            icon: Icon(MyIcon.camera_add),
             onPressed: () {},
           )
         ],
@@ -39,30 +40,140 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _buildBody(BuildContext context) {
-    return ListView.builder(
-      itemCount: _diaryDataList.length,
-      itemBuilder: _buildItem,
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 5.0),
+      child: ListView.builder(
+        itemCount: _diaryDataList.length,
+        itemBuilder: _buildItem,
+      ),
     );
   }
 
   Widget _buildItem(BuildContext context, int index) {
     // DiaryModel diary = _diaryDataList[index];
-    return Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[Text('05月19日'), Text('1岁5个月11天')],
+    return Padding(
+      padding: EdgeInsets.only(top: 30.0),
+      child: Column(
+        children: <Widget>[
+          dateAndAge('05月19日', '1岁5个月11天'),
+          contentBuild(),
+        ],
+      ),
+    );
+  }
+
+  contentBuild() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      margin: EdgeInsets.only(left: 3.0),
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(width: 4.0, color: Colors.orange[700]),
         ),
-        Column(
+      ),
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        color: Colors.white,
+        child: Column(
           children: <Widget>[
-            Text('第一次去动物园'),
-            Text('小老斧(虎)、西(狮)子、二(鳄)鱼、大胸(象)。。。。。'),
-            // Image.network(diary.images[0])
+            contentText(
+                '小老斧(虎)、西(狮)子、二(鳄)鱼,小老斧(虎)、西(狮)子、二(鳄)鱼小老斧(虎)、西(狮)子、二(鳄)鱼小老斧(虎)、西(狮)子、二(鳄)鱼小老斧(虎)、西(狮)子、二(鳄)鱼小老斧(虎)、西(狮)子、二(鳄)鱼小老斧(虎)、西(狮)子、二(鳄)鱼'),
+            creatorInfo('爸爸', '1天前'),
           ],
-        )
+        ),
+      ),
+    );
+  }
+
+  dateAndAge(String diaryDate, String ageDays) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 10.0),
+      child: Row(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: Icon(
+              Icons.panorama_fish_eye,
+              size: 14.0,
+              color: Colors.orange[700],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: Text(
+                    diaryDate,
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400),
+                  ),
+                ),
+                Text(
+                  ageDays,
+                  style: TextStyle(fontSize: 16.0),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  contentText(String contentText) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Text(
+            contentText,
+            textAlign: TextAlign.left,
+          ),
+        ),
       ],
     );
   }
 
+  creatorInfo(String user, String createTime) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              greyText(user),
+              greyText(','),
+              greyText(createTime),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.thumb_up,
+                  color: Colors.grey,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  MyIcon.commnet,
+                  color: Colors.grey,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  greyText(String text) {
+    return Text(text, style: TextStyle(color: Colors.grey));
+  }
   // _retrieveData() {
   //   var urlList = [
   //     'http://09imgmini.eastday.com/mobile/20190214/20190214071055_64630101afc59aa304cebb0d84cfffb5_1.jpeg'
